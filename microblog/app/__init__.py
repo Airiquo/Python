b8dcaddc9361f2df5b448efe433fd50cbdad19a0
Airiquo <sjohnpaul012@gmail.com>
2026-07-17 09:45:12 +0800

    Latest

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -1,10 +1,15 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
 app.config['SECRET_KEY'] = 'you-will-never-guess'
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 #... add more variables here as needed
 
-from app import routes
+from app import routes, models 
 
